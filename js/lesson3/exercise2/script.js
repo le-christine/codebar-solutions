21b65d3527366e566ab6243393dc763998417e88
Christine Le <c.le@celmatix.com>
2019-05-22 13:19:54 -0400

    restrict no. display boxes change preview box on mouse over

--- js/lesson3/exercise2/script.js
+++ js/lesson3/exercise2/script.js
@@ -1,6 +1,6 @@
 /* Exercise 2: Color picker */
 var colors = ['ffac81', 'ff928b', 'fec3a6', 'efe9ae', 'cdeac0']
-
+var previewColor;
 function setPreviewColor(color) {
   $('.preview').css('background-color', color);
 }
@@ -19,6 +19,12 @@ $(document).on('click', '#add-to-favorite', function() {
   addBox(color);
   $('#color').val('');
   $('#color').focus();
+
+  var numColors = $('#colors .item');
+
+  if (numColors.length > 16) {
+    numColors.last().remove();
+  }
 })
 
 $(document).ready(function() {
@@ -28,4 +34,15 @@ $(document).ready(function() {
   });
   var randomColorPick = Math.floor(Math.random() * colors.length);
   setPreviewColor(colors[randomColorPick]);
+  previewColor = colors[randomColorPick];
+
 });
+
+$(document).on('mouseenter', '#colors .item', function() {
+  var previewColor = $(this).css('background-color');
+  setPreviewColor(previewColor);
+})
+
+$(document).on('mouseleave', '#colors .item', function() {
+  setPreviewColor(previewColor);
+})
